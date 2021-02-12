a9d315b006043fbd4f901a73c8268c77e4840201
isabellaeg <isabellaesquivelg@gmail.com>
2021-02-12 19:26:42 -0300

    .

--- src/components/Card/Progress.jsx
+++ src/components/Card/Progress.jsx
@@ -25,45 +25,38 @@ const Progress = ({ data }) => {
   return (
     <Grid item container spacing={1} xs={12}>
       <Grid item>
-        {data.esperandoCorreccion && <HourglassFullIcon fontSize='medium' />}
+        {data.inReview && <HourglassFullIcon fontSize='medium' />}
         {data.progress === 100 && !data.inReview && (
           <CheckCircleIcon
-            variant='static'
-            style={{ color: '#50D174' }}
-            size={20}
+            variant='static' style={{ color: '#50D174' }} size={20}
           />
         )}
 
         {data.progress !== 100 && !data.inReview && (
           <CircularProgress
-            variant='static'
-            value={data.progress}
-            style={{ color: '#50D174' }}
-            size={20}
+            variant='static' value={data.progress} style={{ color: '#50D174' }} size={20}
           />
         )}
       </Grid>
       <Grid item>
-        {data.progress === 100 && !data.inReview && (
-          <Typography className={classes.darkText}>Actividad Completada!</Typography>
-        )}
-
         {data.progress !== 100 && !data.inReview && (
           <Typography className={classes.lightText}>Progreso</Typography>
         )}
-
+        {data.progress === 100 && !data.inReview && (
+          <Typography className={classes.darkText}>Actividad Completada!</Typography>
+        )}
         {data.inReview && data.progress === 100 && (
           <Typography className={classes.darkText}>Esperando corrección</Typography>
         )}
       </Grid>
       <Grid item>
+        {data.progress > 0 &&
+        data.progress < 100 &&
+        !data.inReview && (
+          <Typography className={classes.darkText}>{`${data.progress}%`}</Typography>
+        )}
         {data.progress === 0 && !data.inReview && (
           <Typography className={classes.lightText}>-</Typography>
-        )}
-        {data.progress > 0 &&
-          data.progress < 100 &&
-          !data.inReview && (
-            <Typography className={classes.darkText}>{`${data.progress}%`}</Typography>
         )}
       </Grid>
     </Grid>
